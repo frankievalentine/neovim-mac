ca9349e9d630ac9616f8a9a5bd55dce7bfcdacd9
Frankie <26727138+frankievalentine@users.noreply.github.com>
2025-01-03 09:23:55 -0800

    Remove some unused plugins from Mason, stats, bat config, and add a gitignore

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local constants = require("constants")
 local config = wezterm.config_builder()
 
 -- Font settings
-config.font_size = 17
+config.font_size = 16
 config.line_height = 1.2
 config.font = wezterm.font("Liga SFMono Nerd Font")
 
